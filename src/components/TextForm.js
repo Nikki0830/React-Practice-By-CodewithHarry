a7683090544642d4306f7a2385034e5e9ab5f79e
Nikita Patel <gniki0830@gmail.com>
2022-04-27 12:53:16 +0530

    react-practice

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,21 +14,32 @@ const TextForm = (props) => {
   const [text, setText] = useState("Enter text here!");
   // setText("xyz");
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="mybox"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="mybox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to Uppercase
-      </button>
-    </div>
+      <div className="container my-3">
+        <h1>Your text summmary</h1>
+        <p>
+          {text.split(" ").length} words {text.length} charachers
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 };
 
